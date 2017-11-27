cf97415779a0ab545d5bb58123544a56bdf78c26
khh <khh@khhdeiMac.local>
2017-11-27 21:00:01 +0800

    add artisan

--- public/index.php
+++ public/index.php
@@ -1,9 +1,25 @@
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
 
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels nice to relax.
+|
+*/
 
-// $app = require __DIR__.'/../bootstrap/app.php';
+require __DIR__.'/../bootstrap/autoload.php';
 
 use Kehuanhuan\Route;
 
