615b2d6b3e03c194bebd16bd2570809e463c11b7
khh <khh@khhdeiMac.local>
2017-12-19 20:23:24 +0800

    fix service provider

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -36,10 +36,6 @@ $app->singleton(
     App\Console\Kernel::class
 );
 
-$app->singleton('router',
-    Kehuanhuan\Routing\Router::class
-);
-
 // $app->singleton(
 //     Illuminate\Contracts\Debug\ExceptionHandler::class,
 //     App\Exceptions\Handler::class
